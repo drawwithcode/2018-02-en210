c0cd80cc903c8930fd21a4e5fa2ebc387240dacf
Ennio Sorrentino <enniosorretntino@gmail.com>
2018-10-02 09:41:05 +0200

    new assignement

--- sketch.js
+++ sketch.js
@@ -1,11 +1,43 @@
-function preload(){
-  // put preload code here
+//lection 2
+
+function setup(){
+  createCanvas(windowWidth,windowHeight)
+  background(200)
+  angleMode(DEGREES)
+}
+function draw(){
+  strokeWeight(2)
+
+
+translate(windowWidth/2,windowHeight/2)
+    stroke(0,0,0)
+
+frameRate(25)
+rotate(frameCount)
+                    for(a=0;a<=36000;a++){
+                      if(mouseX>100){
+                        rotate(frameCount)
+                      stroke(mouseX/2,255-mouseX%255,mouseY%255)
+                      strokeWeight(2)
+                      if(a<=frameCount){
+                      line(mouseX*cos(a*32),-mouseX*sin(a*32),100,0)}
+                            }
+                      else {stroke(mouseX/2,255-mouseX%255,mouseY%255)
+                          line(-100*cos(a*30),-100*sin(a*30),100,0)
+                      }
+
+                    }
+
 }
 
-function setup() {
-  // put setup code here
-}
 
-function draw() {
-  // put drawing code here
+function polygon(x, y, radius, npoints) {
+  var angle = TWO_PI / npoints;
+  beginShape();
+  for (var a = 0; a < TWO_PI; a += angle) {
+    var sx = x + cos(a) * radius;
+    var sy = y + sin(a) * radius;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
 }
